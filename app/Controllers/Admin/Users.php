b870e2e4aa75ca3021c99543996be76ff641619f
kancil-nakal <kancilnakal@protonmail.com>
2021-07-07 19:56:26 +0700

    mempelajari routing dan controllers

---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+	public function index()
+	{
+		echo "ini controler Users metod index";
+	}
+
+
+}
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -14,8 +14,8 @@
 			transition: background-color 300ms ease, color 300ms ease;
 		}
 		*:focus {
-			background-color: rgba(221, 72, 20, .2);
 			outline: none;
+			background-color: rgba(221, 72, 20, .2);
 		}
 		html, body {
 			color: rgba(33, 37, 41, 1);
