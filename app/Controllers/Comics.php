c421bbd88d85ccb56dbfd4540fa3c6789e35eead
kancil-nakal <kancilnakal@protonmail.com>
2021-07-16 16:59:54 +0700

    latihan upload gambar

--- app/Controllers/Comics.php
+++ app/Controllers/Comics.php
@@ -61,10 +61,31 @@ class Comics extends BaseController
 					'required' => '{field} komik harus diisi.',
 					'is_unique' => '{field} komik sudah ada.'
 				]
+			],
+			'sampul' => [
+				'rules' => 'max_size[sampul,1024]|is_image[sampul]|mime_in[sampul,image/jpg,image/jpeg,image/png]',
+				'errors' => [
+					'max_size' => 'Ukuran gambar terlalu besar',
+					'is_image' => 'yang ada pilih bukan gambar',
+					'mime_in' => 'yang ada pilih bukan gambar'
+				]
 			]
 		])) {
-			$validation = \Config\Services::validation();
-			return redirect()->to('/comics/create')->withInput()->with('validation', $validation);
+			// $validation = \Config\Services::validation();
+			// return redirect()->to('/comics/create')->withInput()->with('validation', $validation);
+			return redirect()->to('/comics/create')->withInput();
+		}
+
+		//ambil gambar
+		$fileSampul = $this->request->getFile('sampul');
+		//apakah tidak ada gambar yang diupload
+		if ($fileSampul->getError() == 4) {
+			$namaSampul = 'default.jpg';
+		} else {
+			//generate nama sampul random
+			$namaSampul = $fileSampul->getRandomName();
+			//pindahkan ke folder img
+			$fileSampul->move('img', $namaSampul);
 		}
 
 		$slug = url_title($this->request->getVar('judul'), '-', true);
@@ -74,7 +95,7 @@ class Comics extends BaseController
 			'slug' => $slug,
 			'penulis' => $this->request->getVar('penulis'),
 			'penerbit' => $this->request->getVar('penerbit'),
-			'sampul' => $this->request->getVar('sampul')
+			'sampul' => $namaSampul
 		]);
 
 		session()->setFlashdata('pesan', 'Data berhasil ditambahkan!');
@@ -84,6 +105,15 @@ class Comics extends BaseController
 
 	public function delete($id)
 	{
+		//cari gambar berdasarkan id
+		$comics = $this->comicsModel->find($id);
+
+		//cek juga file gambar default
+		if ($comics['sampul'] != 'default.jpg') {
+			//hapus gambar
+			unlink('img/' . $comics['sampul']);
+		}
+
 		$this->comicsModel->delete($id);
 		session()->setFlashdata('pesan', 'Data berhasil dihapus!');
 		return redirect()->to('/comics');
@@ -109,11 +139,33 @@ class Comics extends BaseController
 					'required' => '{field} komik harus diisi.',
 					'is_unique' => '{field} komik sudah ada.'
 				]
+			],
+			'sampul' => [
+				'rules' => 'max_size[sampul,1024]|is_image[sampul]|mime_in[sampul,image/jpg,image/jpeg,image/png]',
+				'errors' => [
+					'max_size' => 'Ukuran gambar terlalu besar',
+					'is_image' => 'yang ada pilih bukan gambar',
+					'mime_in' => 'yang ada pilih bukan gambar'
+				]
 			]
 		])) {
-			$validation = \Config\Services::validation();
-			return redirect()->to('/comics/edit')->withInput()->with('validation', $validation);
+			return redirect()->to('/comics/edit' . $this->request->getVar('slug'))->withInput();
 		}
+
+		$fileSampul = $this->request->getFile('sampul');
+
+		//cek gambar apakah tetap pakai gambar lama
+		if ($fileSampul->getError() == 4) {
+			$namaSampul = $this->request->getVar('sampulLama');
+		} else {
+			//generate file random
+			$namaSampul = $fileSampul->getRandomName();
+			//pindahkan gambar
+			$fileSampul->move('img', $namaSampul);
+			//hapus file lama
+			unlink('img/' . $this->request->getVar('sampulLama'));
+		}
+
 		$slug = url_title($this->request->getVar('judul'), '-', true);
 
 		$this->comicsModel->save([
@@ -122,7 +174,7 @@ class Comics extends BaseController
 			'slug' => $slug,
 			'penulis' => $this->request->getVar('penulis'),
 			'penerbit' => $this->request->getVar('penerbit'),
-			'sampul' => $this->request->getVar('sampul')
+			'sampul' => $namaSampul
 		]);
 
 		session()->setFlashdata('pesan', 'Data berhasil diubah!');
--- app/Views/comics/edit.php
+++ app/Views/comics/edit.php
@@ -5,9 +5,10 @@
     <div class="row">
         <div class="col-8">
             <h2 class="my-3">Form Ubah Data Komik</h2>
-            <form action="/comics/update/<?= $comics['id']; ?>" method="post">
+            <form action="/comics/update/<?= $comics['id']; ?>" method="post" enctype="multipart/form-data">
                 <?= csrf_field(); ?>
                 <input type="hidden" name="slug" value="<?= $comics['slug']; ?>">
+                <input type="hidden" name="sampulLama" value="<?= $comics['sampul']; ?>">
                 <div class="form-group row">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
@@ -31,8 +32,17 @@
                 </div>
                 <div class="form-group row">
                     <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="sampul" name="sampul" value="<?= (old('sampul')) ? old('sampul') : $comics['sampul']; ?> ">
+                    <div class="col-sm-2">
+                        <img src="/img/<?= $comics['sampul']; ?>" class="img-thumbnail img-preview">
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" id="sampul" name="sampul" onchange="previewImg();">
+                            <div id="validationServer03Feedback" class="invalid-feedback">
+                                <?= $validation->getError('sampul') ?>
+                            </div>
+                            <label class="custom-file-label" for="sampul"><?= $comics['sampul']; ?></label>
+                        </div>
                     </div>
                 </div>
                 <div class="form-group row">
